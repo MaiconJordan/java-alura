4013d233cd5bf4d5b5d1d96fdfb42e6a8ab37498
MaiconJordan <maiconplus@gmail.com>
2024-12-15 16:34:25 -0300

    Projeto inicial de OO

---
+++ src/screenmatch/Filme.java
@@ -0,0 +1,29 @@
+/*
+ * @(#)Filme.java
+ *
+ * Copyright (c) J-Tech Solucoes em Informatica.
+ * All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of J-Tech.
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with J-Tech.
+ */
+package screenmatch;
+
+/**
+ * Class Filme
+ *
+ * @author maicon.rocha
+ */
+public class Filme {
+
+    String nome;
+    int anodeLancamento;
+    boolean incluidoNoPlano;
+    double avaliacao;
+    int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+
+
+}
